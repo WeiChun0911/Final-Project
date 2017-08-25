972d3dc45765eff87255abfeb4181610b7af7ae1
wl00887404 <wl00887404@gmail.com>
2017-08-26 00:22:44 +0800

    remove all

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import roomList from "./roomList";
-import participantList from "./participantList";
-import connection from "./connection";
-
-export {
-    roomList,
-    participantList,
-    connection
-}
